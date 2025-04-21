b00319fd1ae2675f6ca6d72a375b953522136260
AbdAlrhman-Almukhtar <Almukhtar20004@yahoo.com>
2025-04-21 23:20:13 +0300

    Polish Our Growth Section

--- src/components/OurGrowth.tsx
+++ src/components/OurGrowth.tsx
@@ -43,24 +43,23 @@ const OurGrowth: React.FC = () => {
       <img
         src={LeftShape}
         alt="Left decoration"
-        className="absolute top-7 left-0 w-26 md:w-28 z-0"
+        className="absolute top-7 left-0 w-26"
       />
       <img
         src={DotsShape}
         alt="Dots decoration"
-        className="absolute top-10 right-10 w-24 md:w-28 z-0"
+        className="absolute top-10 right-10 w-32"
       />
       <img
         src={RightShape}
         alt="Right decoration"
-        className="absolute right-0 bottom-0 w-48 md:w-56 z-0"
+        className="absolute right-0 bottom-0 w-48 md:w-56"
       />
 
       <div className="relative z-10 max-w-6xl mx-auto text-center">
         <h2 className="text-5xl font-bold text-gray-800">Our Growth</h2>
         <p className="mt-3 text-gray-600 text-base md:text-lg max-w-2xl mx-auto">
-          Blocks are symbols that consist of many Elements and are used to
-          quickly build your design.
+        Empowering Student Developers Through Community and Learning
         </p>
 
         <div className="mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 ml-10">
