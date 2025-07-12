486f4a60a6df75e96d12d7a809a16395ee9e9a8d
Tasneem Alsasa <125818114+TasneemAnas05@users.noreply.github.com>
2025-07-12 04:25:07 +0300

    fixes (#14)

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,22 @@ import "./index.css";
 import Navbar from "./components/Navbar";
 import OurGrowth from "./layout/OurGrowth";
 import Footer from "./layout/Footer";
-import Events from "./layout/Events"
+import Events from "./layout/Events";
 import FAQsection from "./components/FAQsection";
 import VisionAndMission from "./layout/VisionAndMission";
 import TeamMemebers from "./layout/TeamMemebers";
+import Closing from "./components/Closing";
+import Hero from "./components/Hero";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Navbar />
-    <OurGrowth/>
-    <Events/>
-    <VisionAndMission/>
-    <TeamMemebers/>
-    <FAQsection/>
-    <Footer/>
+    <Hero />
+    <OurGrowth />
+    <Events />
+    <VisionAndMission />
+    <TeamMemebers />
+    <FAQsection />
+    <Closing />
+    <Footer />
   </StrictMode>
 );
