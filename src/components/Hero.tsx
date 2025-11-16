7a41300b00c075c927f218edd87dab9430790eca
Tasneem Alsasa <125818114+TasneemAnas05@users.noreply.github.com>
2025-11-16 16:22:54 +0300

    update hero video (#17)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import BackgroundVideo from "../assets/videos/backgroundVideo.mp4";
+import BackgroundVideo from "../assets/videos/2024_2025 Recap.mp4";
 const Hero = () => {
   return (
     <div className="relative w-full h-screen overflow-hidden flex items-center justify-center">
@@ -13,16 +13,16 @@ const Hero = () => {
         Your browser does not support the video tag.
       </video>
       <div className="relative z-10 text-white text-center p-5 bg-blue bg-opacity-50 rounded-lg max-w-2xl mx-auto">
-        <h1 className="text-4xl font-bold mb-4 ">
+        {/* <h1 className="text-4xl font-bold mb-4 ">
           Empowering Developers and Fostering Innovation.
         </h1>
         <p className="text-lg mb-6">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
           eiusmod tempor incididunt.
-        </p>
-        <button className="bg-blue-600 hover:bg-blue-700 text-white py-3 px-6 rounded-l rounded-r text-lg shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+        </p> */}
+        {/* <button className="bg-blue-600 hover:bg-blue-700 text-white py-3 px-6 rounded-l rounded-r text-lg shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
           Learn More
-        </button>
+        </button> */}
       </div>
     </div>
   );
